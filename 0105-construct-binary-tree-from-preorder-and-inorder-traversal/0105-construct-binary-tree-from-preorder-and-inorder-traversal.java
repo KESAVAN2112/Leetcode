6abd1de2aec78dbbbe386a3ab69055b14d14eec2
KESAVAN <kesavankannan2005@gmail.com>
2026-09-02 16:03:05 +0530

    Time: 3 ms (29.51%), Space: 45.4 MB (88.62%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -1,29 +1,20 @@
 class Solution {
-    int[] idxData;
-    int idx;
-    
-    public TreeNode getTree(int[] preorder, int l, int r) {
-        if (l >= r)
-            return null;
-        
-        int node = preorder[this.idx++];
-        TreeNode n = new TreeNode(node);
-        int nodeIdx = this.idxData[node + 3000];    
-        
-        n.left = getTree(preorder, l, nodeIdx);
-        n.right = getTree(preorder, nodeIdx + 1, r);
-        
-        return n;
-    }
-    
+    int index = 0;
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-        this.idxData = new int[6001];
-        this.idx = 0;
-           
-        for (int i = 0; i < inorder.length; i++)
-            this.idxData[inorder[i] + 3000] = i;
-        
-        TreeNode root = getTree(preorder, 0, preorder.length);
+        return createTree(preorder, inorder, 0, inorder.length - 1);
+    }
+    public TreeNode createTree(int[] preorder, int[] inorder, int start, int end) {
+        if (start > end) {
+            return null;
+        }
+        int value = preorder[index++];
+        TreeNode root = new TreeNode(value);
+        int position = start;
+        while (inorder[position] != value) {
+            position++;
+        }
+        root.left = createTree(preorder, inorder, start, position - 1);
+        root.right = createTree(preorder, inorder, position + 1, end);
         return root;
     }
 }
